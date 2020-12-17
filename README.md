dc19f22e45b28e4ead686a2c4916e859f4249ea4
Haoshen Han <hanhaoshen@bytedance.com>
2020-12-17 18:56:55 +0800

    open reference

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ the [idea](https://chaopeng.me/blog/2017/03/02/Chrome-Dev-Tools.html) from
 navigate your chromium code to website source chromium
 
 useage:
-1. right-click in your vsc(or select a key word), choose `open in chromium-source website` , it will navigate to corresponding file in source.chroiumu.com
-2. cmd `accept request let browser code open in vscode`, it start local node server on :8989 that receive request from (chrome extension)[https://github.com/han837951112/chromium-open-ide] to open file in vscode. 
+1. right-click in your vsc(select nothing or a key word), choose `open in chromium-source website` , it will navigate to corresponding file in source.chroiumu.com
+2. cmd `accept request let browser code open in vscode`, it start local node server on :8989 that receive request from [chrome extension](https://github.com/han837951112/chromium-open-ide) to open file in vscode. 
+3. select a key word and, choose `try to open References on selected` , it will navigate to corresponding file in source.chroiumu.com and try to open the reference on your key word(just trigger click event, and you should install my chrome plugin mentioned in 2 to make it success)
 
 note that we assume you a in a dir start with src/ (chromium source), and default branch is master
 
@@ -14,7 +15,8 @@ we put log into /tmp/chrome_source_opener.log, sadly the server can't stop once 
 
 用法:
 1. 在src/ 里的代码文件中右键, 选择`open in chromium-source website`, 会在source.chromium.com打开对应的文件
-2. 命令`accept request let browser code open in vscode`, 在8989短裤启动node服务器, 接受来自(chrome 扩展)[https://github.com/han837951112/chromium-open-ide]的请求, 它可以从网站跳转到vsc
+2. 命令`accept request let browser code open in vscode`, 在8989端口启动node服务器, 接受来自(chrome 扩展)[https://github.com/han837951112/chromium-open-ide]的请求, 它可以从网站跳转到vsc
+3. 选中关键词, 右键点击, 选择`try to open References on selected`, 导航到网站上,并尝试触发关键字对应索引(通过2.中chrome插件触发关键字对应点击事件)
 
 注意, 默认你的代码根目录是chromium 的 src, 且跳转到的分支是master
 
--- extension.js
+++ extension.js
@@ -8,75 +8,91 @@ const morgan = require('morgan');
 const fs = require('fs');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+// 集成 chrome 打开 ide 的插件
+function startServer() {
+	let started = false;
+	if (!started) {
+		vscode.window.showInformationMessage('open server in localhost:8989, check log in /tmp/chrome_source_opener.log');
+		started = true;
 
+		var app = express();
+		var logStream = fs.createWriteStream('/tmp/chrome_source_opener.log', { flags: 'a' })
+		app.use(morgan('short', { stream: logStream }))
+
+		app.get('/file', (req, res) => {
+			let filepath = req.query.f;
+			let line = req.query.l;
+			console.log(`open file: ${filepath}:${line}`);
+			exec(`myeditor -f ${filepath} -l ${line}`)
+			res.send("ok");
+		})
+		app.listen(8999);
+	} else {
+		vscode.window.showWarningMessage('server already start');
+	}
+}
+
+function goWeb(isQuery) {
+	const editor = vscode.window.activeTextEditor;
+
+	if (!editor) {
+		console.log('no in a valid editor');
+		return;
+	}
+	const baseUrl = "https://source.chromium.org/chromium/chromium/src/+/master:";
+	var path = editor.document.uri.fsPath
+	var src_idx = path.search('src/')
+	if (src_idx == -1) {
+		console.log('not in a src file')
+		return
+	}
+	path = path.substring(src_idx + 4)
+	var line = (editor.selection.active.line + 1).toString()
+	const selection = vscode.window.activeTextEditor.selection
+	const selected = vscode.window.activeTextEditor.document.getText(new vscode.Range
+		(selection.start, selection.end))
+	let queryUrl = `${baseUrl}${path};l=${line}`
+	if (!selection.isEmpty) {
+		if (isQuery) {
+			queryUrl += `;query=${selected}`
+		} else {
+			queryUrl += `?q=${selected}`
+		}
+	}
+	open(queryUrl)
+}
+function openWeb() {
+	goWeb(false)
+}
+function searchWeb() {
+	goWeb(true)
+}
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// 集成 chrome 打开 ide 的插件
-	var app = express();
-	var logStream = fs.createWriteStream('/tmp/chrome_source_opener.log', {flags: 'a'})
-	app.use(morgan('short', {stream: logStream}))
-
-	app.get('/file', (req, res)=> {
-		let filepath = req.query.f;
-		let line = req.query.l ;
-		console.log(`open file: ${filepath}:${line}`);
-		exec(`myeditor -f ${filepath} -l ${line}`)
-		res.send("ok");
-	})
-	let started = false;
-	let listen = vscode.commands.registerCommand('chromium-source-opener.listen', function() {
-		if (!started) {
-			vscode.window.showInformationMessage('open server in localhost:8989, check log in /tmp/chrome_source_opener.log');
-			started = true;
-			app.listen(8999);
-		} else {
-			vscode.window.showWarningMessage('server already start');
-		}
-	})
+	let listen = vscode.commands.registerCommand('chromium-source-opener.listen', startServer)
 	/*
 	// how to stop this?
 	vscode.commands.registerCommand('chromium-source-opener.unlisten', function() {
 
 	})
 	*/
-	
-	//  try to open in source.chromium.org
-	let disposable = vscode.commands.registerCommand('chromium-source-opener.openInWeb', async function () {
-		const editor = vscode.window.activeTextEditor;
 
-		if (!editor)	{
-			console.log('no in a valid editor');
-			return ;
-		}
-		const baseUrl = "https://source.chromium.org/chromium/chromium/src/+/master:";
-		var path = editor.document.uri.fsPath
-		var src_idx = path.search('src/')
-		if (src_idx == -1) {
-			console.log('not in a src file')
-			return 
-		}
-		path = path.substring(src_idx + 4 )
-		var line = (editor.selection.active.line + 1).toString()
-		// console.log(path, line)
-		const selection = vscode.window.activeTextEditor.selection
-		const selected = vscode.window.activeTextEditor.document.getText(new vscode.Range
-			(selection.start, selection.end))
-		let queryUrl = `${baseUrl}${path};l=${line}`
-		if (!selection.isEmpty) {
-			queryUrl += `?q=${selected}`
-		}
-		await open(queryUrl)
-	});
+	//  try to open in source.chromium.org
+	let openweb = vscode.commands.registerCommand('chromium-source-opener.openInWeb', openWeb);
+	let searchweb = vscode.commands.registerCommand('chromium-source-opener.searchInWeb', searchWeb);
+
+
 
 	context.subscriptions.push(listen);
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(openweb);
+	context.subscriptions.push(searchweb);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
